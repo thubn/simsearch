49ce001e7e8d58c05b460752671cb2434cb7fbc7
thubn <thorb.simon@gmail.com>
2024-11-03 22:02:53 +0100

    calculate norm for queries, fix optimized embeddings for python bindings

--- src/embedding_search_bindings.cpp
+++ src/embedding_search_bindings.cpp
@@ -1,4 +1,5 @@
 // embedding_search_bindings.cpp
+#include "config_manager.h"
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include <pybind11/numpy.h>
@@ -6,9 +7,13 @@
 #include "embedding_search_float.h"
 #include "embedding_search_binary_avx2.h"
 #include "embedding_search_uint8_avx2.h"
+#include "optimized_embedding_search_avx2.h"
+#include "optimized_embedding_search_uint8_avx2.h"
+#include "optimized_embedding_search_binary_avx2.h"
 #include "embedding_utils.h"
 #include <chrono>
 #include <immintrin.h>
+#include <iostream>
 
 namespace py = pybind11;
 
@@ -16,25 +21,52 @@ class PyEmbeddingSearch
 {
 private:
     // EmbeddingSearchFloat float_searcher;
-    EmbeddingSearchAVX2 avx2_searcher;
-    EmbeddingSearchBinaryAVX2 binary_avx2_searcher;
-    EmbeddingSearchUint8AVX2 uint8_avx2_searcher;
+    std::unique_ptr<OptimizedEmbeddingSearchAVX2> avx2_searcher;
+    std::unique_ptr<OptimizedEmbeddingSearchBinaryAVX2> binary_avx2_searcher;
+    std::unique_ptr<OptimizedEmbeddingSearchUint8AVX2> uint8_avx2_searcher;
     bool is_initialized = false;
+    std::string config_path;
 
 public:
+    PyEmbeddingSearch()
+    {
+        // Just store the config path in constructor, don't initialize anything yet
+        config_path = std::filesystem::path(py::module::import("embedding_search").attr("__file__").cast<std::string>())
+                          .parent_path()
+                          .string() +
+                      "/config.json";
+        // ensure_config_exists(config_path);
+    }
+
     bool load(const std::string &filename)
     {
-        EmbeddingSearchFloat float_searcher;
-        bool success = float_searcher.load(filename);
-        if (success)
+        try
         {
-            avx2_searcher.setEmbeddings(float_searcher.getEmbeddings());
-            avx2_searcher.setSentences(float_searcher.getSentences());
-            binary_avx2_searcher.create_binary_embedding_from_float(float_searcher.getEmbeddings());
-            uint8_avx2_searcher.setEmbeddings(float_searcher.getEmbeddings());
-            is_initialized = true;
+            // Initialize config manager
+            ConfigManager::getInstance().initialize(config_path);
+
+            avx2_searcher = std::make_unique<OptimizedEmbeddingSearchAVX2>();
+            binary_avx2_searcher = std::make_unique<OptimizedEmbeddingSearchBinaryAVX2>();
+            uint8_avx2_searcher = std::make_unique<OptimizedEmbeddingSearchUint8AVX2>();
+
+            EmbeddingSearchFloat float_searcher;
+            bool success = float_searcher.load(filename);
+            if (success)
+            {
+                avx2_searcher->setEmbeddings(float_searcher.getEmbeddings());
+                avx2_searcher->setSentences(float_searcher.getSentences());
+                binary_avx2_searcher->setEmbeddings(float_searcher.getEmbeddings());
+                uint8_avx2_searcher->setEmbeddings(float_searcher.getEmbeddings());
+                is_initialized = true;
+                float_searcher.unsetEmbeddings();
+                float_searcher.unsetSentences();
+            }
+            return success;
         }
-        return success;
+        catch (const std::exception &e)
+        {
+            throw std::runtime_error(std::string("Error during initialization: ") + e.what());
+        }
     }
 
     py::tuple benchmark_search(py::array_t<float> query_vectors, size_t k, size_t num_runs)
@@ -79,14 +111,14 @@ public:
 
                 // AVX2 search
                 auto start = std::chrono::high_resolution_clock::now();
-                auto avx2_results = avx2_searcher.similarity_search(queryAvx2, k);
+                auto avx2_results = avx2_searcher->similarity_search(queryAvx2, k);
                 auto end = std::chrono::high_resolution_clock::now();
                 avx2_times.push_back(std::chrono::duration_cast<std::chrono::microseconds>(end - start).count());
                 // avx2_accuracies.push_back(EmbeddingUtils::calculateJaccardIndex(float_results, avx2_results));
 
                 // Binary AVX2 search
                 start = std::chrono::high_resolution_clock::now();
-                auto binary_results = binary_avx2_searcher.similarity_search(queryBinaryAvx2, k);
+                auto binary_results = binary_avx2_searcher->similarity_search(queryBinaryAvx2, k);
                 end = std::chrono::high_resolution_clock::now();
                 binary_times.push_back(std::chrono::duration_cast<std::chrono::microseconds>(end - start).count());
                 binary_accuracies.push_back(EmbeddingUtils::calculateJaccardIndex(avx2_results, binary_results));
@@ -122,6 +154,11 @@ public:
             throw std::runtime_error("Query vector must be 1-dimensional");
         }
         std::vector<float> query(static_cast<float *>(buf.ptr), static_cast<float *>(buf.ptr) + buf.shape[0]);
+        float norm = EmbeddingUtils::calcNorm(query);
+        for (float &val : query)
+        {
+            val = val / norm;
+        }
 
         try
         {
@@ -140,26 +177,26 @@ public:
 
             // Perform AVX2 search with timing
             auto avx2_start = std::chrono::high_resolution_clock::now();
-            auto avx2_results = avx2_searcher.similarity_search(queryAvx2, k);
+            auto avx2_results = avx2_searcher->similarity_search(query, k);
             auto avx2_end = std::chrono::high_resolution_clock::now();
             avx2_time = std::chrono::duration_cast<std::chrono::microseconds>(avx2_end - avx2_start).count();
 
             // Perform Binary AVX2 search with timing
             auto binary_start = std::chrono::high_resolution_clock::now();
-            auto binary_results = binary_avx2_searcher.similarity_search(queryBinaryAvx2, k);
+            auto binary_results = binary_avx2_searcher->similarity_search(queryBinaryAvx2, k);
             auto binary_end = std::chrono::high_resolution_clock::now();
             binary_time = std::chrono::duration_cast<std::chrono::microseconds>(binary_end - binary_start).count();
 
             // Perform UINT8 AVX2 search with timing
             auto uint8_start = std::chrono::high_resolution_clock::now();
-            auto uint8_results = uint8_avx2_searcher.similarity_search(queryUint8Avx2, k);
+            auto uint8_results = uint8_avx2_searcher->similarity_search(queryUint8Avx2, k);
             auto uint8_end = std::chrono::high_resolution_clock::now();
             uint8_time = std::chrono::duration_cast<std::chrono::microseconds>(uint8_end - uint8_start).count();
 
             // Perform Binary AVX2 search with timing
             auto twostep_start = std::chrono::high_resolution_clock::now();
-            auto twostep_binary_results = binary_avx2_searcher.similarity_search(queryBinaryAvx2, k * rescoring_factor);
-            auto twostep_avx2_results = avx2_searcher.similarity_search(queryAvx2, k, twostep_binary_results);
+            auto twostep_binary_results = binary_avx2_searcher->similarity_search(queryBinaryAvx2, k * rescoring_factor);
+            auto twostep_avx2_results = avx2_searcher->similarity_search(query, k, twostep_binary_results);
             auto twostep_end = std::chrono::high_resolution_clock::now();
             twostep_time = std::chrono::duration_cast<std::chrono::microseconds>(twostep_end - twostep_start).count();
 
@@ -173,7 +210,7 @@ public:
             double twostep_ndcg = EmbeddingUtils::calculateNDCG(avx2_results, twostep_avx2_results);
 
             // Get sentences
-            const auto &sentences = avx2_searcher.getSentences();
+            const auto &sentences = avx2_searcher->getSentences();
 
             // Convert results to Python lists
             py::list avx2_results_py, binary_results_py, uint8_results_py, twostep_results_py;
